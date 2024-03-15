f4d11eaee442ea67f42b054a7aeaf2c2f33617e7
Divya R <kwrdivyaraj100@gmail.com>
2024-03-15 16:11:19 +0530

    Adding logic to increment counter based on action payload

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,20 @@ function App() {
   };
 
   function decrement() {
-
+    dispatch({ type: 'DEC' });
   };
+
+  function addBy() {
+    dispatch({ type: 'ADD', payload: 10 })
+  };
+
   return (
     <div >
       <h1>Counter App</h1>
       <h2>{counter}</h2>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
+      <button onClick={addBy}>Add by 10</button>
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,15 @@ const reducerFn = (state = { counter: 0 }, action) => {
     if (action.type === "INC") {
         return { counter: state.counter + 1 };
     }
+
+    if (action.type === "DEC") {
+        return { counter: state.counter - 1 };
+    }
+
+    if (action.type === "ADD") {
+        return { counter: state.counter + action.payload };
+    }
+
     return state;
 };
 
