66f6d88bbfbba1b7302d7ab94fc59ffa71f90308
Divya R <kwrdivyaraj100@gmail.com>
2024-03-15 15:47:43 +0530

    Adding basic Redux store setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import './App.css';
+import { UseSelector, useSelector } from "react-redux";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
   return (
     <div >
-
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
     </div>
   );
 }
